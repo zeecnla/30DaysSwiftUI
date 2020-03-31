4517ab100f419583aa5f361549354d6b9a24246b
zeecnla <csrmelchor@gmail.com>
2020-03-31 12:45:39 -0700

    Initial Commit

---
+++ OneSecond-Day1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OneSecond-Day1
+//
+//  Created by Cesar Melchor on 3/31/20.
+//  Copyright © 2020 Cesar Melchor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
