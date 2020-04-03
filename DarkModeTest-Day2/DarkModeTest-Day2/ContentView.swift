1c3b4efe2b2d46d68f46597d7925a7c5d9b56f99
zeecnla <csrmelchor@gmail.com>
2020-04-03 11:28:17 -0700

    Day 2
    initial day 2 commit

---
+++ DarkModeTest-Day2/DarkModeTest-Day2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DarkModeTest-Day2
+//
+//  Created by Cesar Melchor on 4/3/20.
+//  Copyright © 2020 Cesar Melchor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DarkModeTest-Day2/DarkModeTest-Day2Tests/DarkModeTest_Day2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  DarkModeTest_Day2Tests.swift
+//  DarkModeTest-Day2Tests
+//
+//  Created by Cesar Melchor on 4/3/20.
+//  Copyright © 2020 Cesar Melchor. All rights reserved.
+//
+
+import XCTest
+@testable import DarkModeTest_Day2
+
+class DarkModeTest_Day2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
