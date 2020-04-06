da59a3449c5d9cbdcd59db67466a7bed1d42ba2f
zeecnla <csrmelchor@gmail.com>
2020-04-06 10:53:48 -0700

    Finished Day 2
    completed challenge of day 2

--- DarkModeTest-Day2/DarkModeTest-Day2/ContentView.swift
+++ DarkModeTest-Day2/DarkModeTest-Day2/ContentView.swift
@@ -9,8 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.colorScheme) var  colorScheme: ColorScheme
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color.init(UIColor(named:"accentColor")!)
+              .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                HStack{
+                    VStack(alignment:.leading){
+                        
+                        Text("Now, is")
+                            .font(.system(size:32, weight:.black))
+                            .foregroundColor(Color(UIColor(named: "labelColor")!))
+                            .multilineTextAlignment(.leading)
+                        Text(colorScheme  == .light ? "Light    " : "Dark   ")
+                            .font(colorScheme == .light ?  Font.custom("Condiment-Regular", size: 98) : Font.custom("Superclarendon", size: 78))
+                            .foregroundColor(Color(UIColor(named: "labelColor")!).opacity(0.75))
+                            .multilineTextAlignment(.leading)
+                            .padding(.top, 20)
+                            .frame(width: 250.0)
+                    }
+                    .padding(.trailing, 80)
+                }
+                Spacer()
+                    .frame(height: 20, alignment: <#T##Alignment#>)
+                Text("made by @zeecnla")
+                .font(.system(size:16, weight:.black))
+                .foregroundColor(Color(UIColor(named: "labelColor")!).opacity(0.75))
+            }
+            .padding()
+        }
     }
 }
 
